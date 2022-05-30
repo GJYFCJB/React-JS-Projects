1df2927ad17a851d42029cf6a4e4bfff7d3b2f10
jinGu <gj1308838@gmail.com>
2022-05-29 22:25:54 -0500

    practice box

---
+++ box-app/src/components/app.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import NavBar from './navbar';
+import Boxes from './boxes';
+
+class App extends Component {
+    state = {
+        boxes: [
+            {id: 1, x: 1},
+            {id: 2, x: 2},
+            {id: 3, x: 3},
+            {id: 4, x: 4},
+        ]
+    }
+
+    handleReset = () => {
+      const boxes = this.state.boxes.map(b => {
+        return {
+          id : b.id,
+          x : 0,
+        }
+      });
+      this.setState({
+          boxes
+      });
+    };
+
+    handleClickLeft = (box) => {
+      const boxes = [...this.state.boxes];
+      const k = boxes.indexOf(box);
+      boxes[k] = {...boxes[k]};//clone origin data and create really new object
+      boxes[k].x--;
+      this.setState({boxes});
+  };
+
+  handleClickRight = (box) => {
+    const boxes = [...this.state.boxes];
+    const k = boxes.indexOf(box);
+    boxes[k] = {...boxes[k]};
+    boxes[k].x++;
+    this.setState({boxes});
+  };
+
+  handleDel = (id) => {
+    const boxes = this.state.boxes.filter((b) => b.id !== id);
+    this.setState({
+      boxes
+    });
+  };
+
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar boxesCount = {this.state.boxes.filter(b => b.x !== 0).length}>
+                    Nav
+                </NavBar>
+                <div className='container'>
+                    <Boxes
+                        boxes={this.state.boxes}
+                        onReset={this.handleReset}
+                        onClickLeft={this.handleClickLeft}
+                        onClickRight={this.handleClickRight}
+                        onDelete={this.handleDel}
+                    />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
